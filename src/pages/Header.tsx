a180965abba3a88722768526f95863a396e5ed3b
koto-ista <kaito.koto@i-standard.jp>
2025-07-17 16:19:09 +0900

    ナビゲーション部分（簡易作成）

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -6,6 +6,14 @@ const Header = () => {
       <div className="header-wrapper">
         <h1 className="logo">ロログ</h1>
         <h2 className="logo-sub">各種名簿管理アプリ</h2>
+        {/* ナビゲーション要素として切り出す */}
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-item"><a href="/">従業員</a></li>
+            <li className="header-nav-item"><a href="/">顧客</a></li>
+            <li className="header-nav-item"><a href="/">取引先</a></li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
